14a1739b2b92090c662accfdb32b900a99d07345
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-01 09:02:13 +0700

    Add Layout
    Add Layout component include MainNavigation component

---
+++ grocery-store/src/components/layout/MainNavigation.js
@@ -0,0 +1,27 @@
+import { Fragment } from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <div className={classes.logo}>
+          Grocery Store
+        </div>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink to='' activeClassName={classes.active}>View Cart</NavLink>
+            </li>
+            <li>
+              <NavLink to='' activeClassName={classes.active}>Login</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </Fragment>
+  );
+};
+
+export default MainNavigation;
