324a3d1e0e1d142cf30ee29e27de7ee9f7ce0823
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-05 16:29:31 +0700

    Add page ,and delete some resource
    Add new page expected. (Not Final)
    Delete resource not use

--- grocery-store/src/App.js
+++ grocery-store/src/App.js
@@ -1,13 +1,25 @@
 import React, { Fragment, Suspense } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom'; c
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+import Home from './pages/Home';
 
 function App() {
   return (
     <Fragment>
       <Layout>
-
+        <Suspense fallback={<div className='centered'><LoadingSpinner /></div>}>
+          <Switch>
+            <Route path='/' exact>
+              <Redirect to='/home' />
+            </Route>
+            <Route path='/home' exact>
+              <Home />
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
     </Fragment>
   );
---
+++ grocery-store/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <div className='centered'>
+        <h2 className='not-found-content'>Page Not Found!!!</h2>
+      </div>
+    </Fragment>
+  );
+};
+
+export default NotFound;
